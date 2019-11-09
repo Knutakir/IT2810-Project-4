0416bfca0b8a0fdf649a69f3f6f3e74045315341
Ingunn Sund <ingunnsu@online.no>
2019-11-10 00:30:10 +0100

    Add background image and fonts (#11)

--- client/App.js
+++ client/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
+
+const windowSize = Dimensions.get('window');
+const background = '#5c4d48';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Hello world!</Text>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Mountain Finder</Text> 
+            </View>
+            <Image style={styles.backgroundImage} source={require('./assets/pink-mountains.jpg')}></Image>
             <Text>Project 4!😎</Text>
+            
         </View>
     );
 }
@@ -13,8 +21,26 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: background,
         alignItems: 'center',
-        justifyContent: 'center',
+    },
+    titleContainer: {
+        position: 'absolute',
+        width: windowSize.width,
+        zIndex: 3,
+        alignItems: 'center',
+    },
+    title: {  
+        fontSize: 70,
+        color: background,
+        fontFamily: 'DancingScript-Bold',
+    },
+    backgroundImage: {
+        width: windowSize.width,
+        height: vh(40),
+        minHeight: 300,
+        resizeMode: 'stretch',   
     },
 });
+
+/* text-shadow: 2px 2px #5c4d4840; */
