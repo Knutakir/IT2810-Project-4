c4131bbcde3a16e755523d061c46545a3d8adf04
Ingunn Sund <ingunnsu@online.no>
2019-11-15 01:07:42 +0100

    Add possibility for clicking outside of modal to close it (#3)

--- client/components/List.js
+++ client/components/List.js
@@ -4,7 +4,10 @@ import {
     Modal,
     StyleSheet,
     TouchableHighlight,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
     Text,
+    ScrollView,
 } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -27,13 +30,24 @@ function List({
                 <ListItem name='Mount Everest' country='Nepal' height={1243} rating='2.4' clickItem={() => clickListItem('Mount Everest')} />
                 <ListItem name='K2' country='Nepal' height={15553} rating='4.4' clickItem={() => clickListItem('K2')} />
             </View>
-            <Modal animationType="slide" transparent={false} visible={modalVisible}>
-                <TouchableHighlight
-                    onPress={() => {
-                        setModalVisible(false);
-                    }}>
-                    <Text>Hide Modal</Text>
-                </TouchableHighlight>
+            { /* Solution for clicking outside of modal to close found here: https://stackoverflow.com/questions/40483034/close-react-native-modal-by-clicking-on-overlay */}
+            <Modal animationType="slide" transparent={false} visible={modalVisible} onRequestClose={() => {setModalVisible(false)}}>
+                <TouchableOpacity style={styles.modalContainer} activeOpacity={1} onPressOut={() => {setModalVisible(false)}}>
+                    <ScrollView directionalLockEnabled={true}>
+                        <View style={styles.modalContainer2}>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.modalContentContainer}>
+                                <TouchableHighlight
+                                    onPress={() => {
+                                        setModalVisible(false);
+                                    }}>
+                                    <Text>Hide Modal</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </TouchableWithoutFeedback>
+                        </View>
+                    </ScrollView>
+                </TouchableOpacity>
             </Modal>
         </View>
     );
@@ -43,6 +57,25 @@ const styles = StyleSheet.create({
     list: {
         width: vw(90),
     },
+    modalContainer: {
+        flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+    },
+    modalContainer2: {
+        /*marginTop: windowSize.height*6/16,*/
+        flex: 1,
+        flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+    },
+    modalContentContainer: {
+        backgroundColor: '#000000',
+	    width: 50,
+		height:50,
+	    borderRadius: 18,
+		opacity: 0.8,
+    }
 });
 
 const mapStateToProps = state => ({
